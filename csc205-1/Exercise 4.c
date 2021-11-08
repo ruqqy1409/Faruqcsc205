5d4f3867def5bdf5f3d9964f4e566aa6d6e80a71
ruqqy1409 <93473680+ruqqy1409@users.noreply.github.com>
2021-11-08 14:11:47 +0100

    Add files via upload

---
+++ csc205-1/Exercise 4.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+
+int main()
+{
+char myInitial = 'D';
+char myName[] = "Desmond Moru";
+printf("your name is = %s\n", myName);
+printf("The initial of your firstname is = %c/n", myInitial);
+return 0;
+}
